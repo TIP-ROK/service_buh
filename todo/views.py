f70ef9852ec22ec0a929641d4204028711f02c45
TIP-ROK <chenye797@gmail.com>
2022-10-10 14:18:22 +0600

    created new wai to represent the result of activity without saving

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, views
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from todo.models import IncomeExpense, IncomeExpenseBalance
 from todo.serializers import IncomeExpensesSerializer, SumSerializer
@@ -12,3 +14,21 @@ class IncomeExpensesViewSet(viewsets.ModelViewSet):
 class IncomeExpenseBalanceViewSet(viewsets.ModelViewSet):
     queryset = IncomeExpenseBalance.objects.all()
     serializer_class = SumSerializer
+
+
+class ResultAPIView(views.APIView):
+    # результат этойго класса сейчас не имеет возможности сохранять данные
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        sum_of_income = sum(IncomeExpense.objects.filter(is_expenses=True, user=request.user).values_list('amount', flat=True))
+        sum_of_expense = sum(IncomeExpense.objects.filter(is_expenses=False, user=request.user).values_list('amount', flat=True))
+        result = sum_of_income - sum_of_expense
+        return Response(
+                (
+                    f'сумма расходов : {sum_of_expense}, '
+                    f'сумма доходов : {sum_of_income}, '
+                    f'результат : {result}'
+                ),
+                status=200
+            )
--- todo_buh/urls.py
+++ todo_buh/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from todo.views import IncomeExpensesViewSet, IncomeExpenseBalanceViewSet
+from todo.views import IncomeExpensesViewSet, IncomeExpenseBalanceViewSet, ResultAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('<int:pk>/', IncomeExpensesViewSet.as_view({'get': 'retrieve', 'put': 'update', 'destroy': 'delete'})),
     path('balance/', IncomeExpenseBalanceViewSet.as_view({'get': 'list'})),
     path('balance/<int:pk>/', IncomeExpenseBalanceViewSet.as_view({'get': 'retrieve'})),
+    path('result', ResultAPIView.as_view())
 ]
